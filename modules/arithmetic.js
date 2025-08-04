d116d7343cc8539d75cddd3d0584ef6dea7c70ea
pmayur02 <mayurpansare2000@gmail.com>
2025-08-04 16:45:28 +0530

    modules concept

---
+++ modules/arithmetic.js
@@ -0,0 +1,15 @@
+module.exports.add=(num1,num2)=>{
+    return num1+num2;
+}
+
+module.exports.sub=(num1,num2)=>{
+    return num1-num2;
+}
+
+module.exports.multi=(num1,num2)=>{
+    return num1*num2;
+}
+
+module.exports.div=(num1,num2)=>{
+    return num1/num2;
+}
---
+++ modules/use.js
@@ -0,0 +1,9 @@
+const { add, sub, multi, div } = require("./arithmetic");
+
+console.log(add(3,5));
+
+console.log(sub(3,5));
+
+console.log(multi(3,5));
+
+console.log(div(3,5));
